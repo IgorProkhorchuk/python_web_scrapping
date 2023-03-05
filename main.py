270538e8f7d1351b0ab2d081e31e65e36b5a4352
Igor Prokhorchuk <igor.prokhorchuk@yahoo.com>
2023-03-05 23:02:35 +0200

    adding a_tag search

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ print(site_page.status_code)
 soup = BeautifulSoup(site_page.content, "html.parser")
 
 jobs = soup.find_all('div', class_ = 'card card-hover card-visited wordwrap job-link js-hot-block')
-file = open("output.txt", "w")
 
+# write jobs variable content to a html file
 output_str = ""
 for row in jobs:
     output_str += str(row) + "\n"
@@ -28,3 +28,16 @@ for row in jobs:
 file = open("output.html", "w")
 file.write(output_str)
 file.close()
+
+# find a job title name
+#for job in jobs:
+job_title_class = soup.find_all('div', class_ = "card card-hover card-visited wordwrap job-link js-hot-block")
+for job_title in job_title_class:
+  job_title_name = job_title.find_all('h2')
+  for h2_tag in job_title_name:
+     job_name = h2_tag.text
+    
+with open('work_ua.csv', "w", newline="") as csv_file:
+    writer = csv.writer(csv_file)
+
+    writer.writerow(str(job_title_name))
